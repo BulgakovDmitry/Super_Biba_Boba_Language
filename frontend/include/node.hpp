0ca0df6310d919e1f13cc753dcbd7db2e445c91d
GitHub Action <action@github.com>
2025-11-13 20:47:39 +0000

    Auto-format code

--- frontend/include/node.hpp
+++ frontend/include/node.hpp
@@ -28,18 +28,17 @@ class ASTVisitor {
   public:
     virtual ~ASTVisitor() = default;
 
-    virtual void visit(Program &node)         = 0;
-    virtual void visit(Block_stmt &node)      = 0;
+    virtual void visit(Program &node) = 0;
+    virtual void visit(Block_stmt &node) = 0;
     virtual void visit(Assignment_stmt &node) = 0;
-    virtual void visit(Input_stmt &node)      = 0;
-    virtual void visit(If_stmt &node)         = 0;
-    virtual void visit(While_stmt &node)      = 0;
-    virtual void visit(Print_stmt &node)      = 0;
+    virtual void visit(Input_stmt &node) = 0;
+    virtual void visit(If_stmt &node) = 0;
+    virtual void visit(While_stmt &node) = 0;
+    virtual void visit(Print_stmt &node) = 0;
     virtual void visit(Binary_operator &node) = 0;
-    virtual void visit(Unary_operator &node)  = 0;
-    virtual void visit(Number &node)          = 0;
-    virtual void visit(Variable &node)        = 0;
-
+    virtual void visit(Unary_operator &node) = 0;
+    virtual void visit(Number &node) = 0;
+    virtual void visit(Variable &node) = 0;
 };
 
 class Node {
@@ -48,16 +47,11 @@ class Node {
     virtual void accept(ASTVisitor &visitor) = 0;
 };
 
-enum class Binary_operators {
-    Eq, Neq, Less, LessEq, Greater, GreaterEq,
-    Add, Sub, Mul, Div
-};
+enum class Binary_operators { Eq, Neq, Less, LessEq, Greater, GreaterEq, Add, Sub, Mul, Div };
 
-enum class Unary_operators {
-    Neg, Plus
-};
+enum class Unary_operators { Neg, Plus };
 
-class Statement  : public Node {};
+class Statement : public Node {};
 class Expression : public Node {};
 class LValueExpr : public Expression {};
 class RValueExpr : public Expression {};
@@ -75,7 +69,7 @@ class Program : public Node {
     explicit Program(StmtList stmts) : stmts_(std::move(stmts)) {}
 
     const StmtList &get_stmts() const { return stmts_; }
-          StmtList &get_stmts()       { return stmts_; }
+    StmtList &get_stmts() { return stmts_; }
 
     void accept(ASTVisitor &visitor) override { visitor.visit(*this); }
 };
@@ -88,7 +82,7 @@ class Block_stmt : public Statement {
     explicit Block_stmt(StmtList stmts) : stmts_(std::move(stmts)) {}
 
     const StmtList &get_stmts() const { return stmts_; }
-          StmtList &get_stmts()       { return stmts_; }
+    StmtList &get_stmts() { return stmts_; }
 
     void accept(ASTVisitor &visitor) override { visitor.visit(*this); }
 };
@@ -103,8 +97,8 @@ class Assignment_stmt : public Statement {
         : variable_(std::move(variable)), value_(std::move(value)) {}
 
     const Variable_ptr &get_variable() const { return variable_; }
-    Expression         &get_value()         { return *value_; }
-    const Expression   &get_value() const   { return *value_; }
+    Expression &get_value() { return *value_; }
+    const Expression &get_value() const { return *value_; }
 
     void accept(ASTVisitor &visitor) override { visitor.visit(*this); }
 };
@@ -112,33 +106,33 @@ class Assignment_stmt : public Statement {
 class While_stmt : public Statement {
   private:
     Expression_ptr condition_;
-    Statement_ptr  body_;
+    Statement_ptr body_;
 
   public:
     While_stmt(Expression_ptr condition, Statement_ptr body)
         : condition_(std::move(condition)), body_(std::move(body)) {}
 
-    Expression &get_condition()      { return *condition_; }
-    Statement  &get_body()           { return *body_; }
+    Expression &get_condition() { return *condition_; }
+    Statement &get_body() { return *body_; }
 
     void accept(ASTVisitor &visitor) override { visitor.visit(*this); }
 };
 
 class If_stmt : public Statement {
   private:
-    Expression_ptr  condition_;
-    Statement_ptr  then_branch_;
-    Statement_ptr  else_branch_; 
+    Expression_ptr condition_;
+    Statement_ptr then_branch_;
+    Statement_ptr else_branch_;
 
   public:
-    If_stmt(Expression_ptr condition, Statement_ptr then_branch, Statement_ptr else_branch = nullptr)
-        : condition_(std::move(condition)),
-          then_branch_(std::move(then_branch)),
+    If_stmt(Expression_ptr condition, Statement_ptr then_branch,
+            Statement_ptr else_branch = nullptr)
+        : condition_(std::move(condition)), then_branch_(std::move(then_branch)),
           else_branch_(std::move(else_branch)) {}
 
-    Expression &condition()        { return *condition_; }
-    Statement  &then_branch()      { return *then_branch_; }
-    Statement  *else_branch()      { return else_branch_.get(); }
+    Expression &condition() { return *condition_; }
+    Statement &then_branch() { return *then_branch_; }
+    Statement *else_branch() { return else_branch_.get(); }
     const Statement *else_branch() const { return else_branch_.get(); }
 
     void accept(ASTVisitor &visitor) override { visitor.visit(*this); }
@@ -149,8 +143,7 @@ class Input_stmt : public Statement {
     Variable_ptr variable_;
 
   public:
-    explicit Input_stmt(Variable_ptr variable)
-        : variable_(std::move(variable)) {}
+    explicit Input_stmt(Variable_ptr variable) : variable_(std::move(variable)) {}
 
     const Variable_ptr &get_variable() const { return variable_; }
 
@@ -160,11 +153,11 @@ class Input_stmt : public Statement {
 class Print_stmt : public Statement {
   private:
     Expression_ptr value_;
-  public:
-    explicit Print_stmt(Expression_ptr value)
-        : value_(std::move(value)) {}
 
-    Expression       &get_value()       { return *value_; }
+  public:
+    explicit Print_stmt(Expression_ptr value) : value_(std::move(value)) {}
+
+    Expression &get_value() { return *value_; }
     const Expression &get_value() const { return *value_; }
 
     void accept(ASTVisitor &visitor) override { visitor.visit(*this); }
@@ -175,14 +168,15 @@ class Binary_operator : public RValueExpr {
     Binary_operators op_;
     Expression_ptr left_;
     Expression_ptr right_;
+
   public:
-    Binary_operator(Binary_operators op, Expression_ptr left, Expression_ptr right) 
+    Binary_operator(Binary_operators op, Expression_ptr left, Expression_ptr right)
         : op_(op), left_(std::move(left)), right_(std::move(right)) {}
 
     Binary_operators get_operator() const { return op_; }
-    Expression   &get_left()       { return *left_; }
-    const Expression &get_left() const  { return *left_; }
-    Expression   &get_right()      { return *right_; }
+    Expression &get_left() { return *left_; }
+    const Expression &get_left() const { return *left_; }
+    Expression &get_right() { return *right_; }
     const Expression &get_right() const { return *right_; }
 
     void accept(ASTVisitor &visitor) override { visitor.visit(*this); }
@@ -192,13 +186,14 @@ class Unary_operator : public RValueExpr {
   private:
     Unary_operators op_;
     Expression_ptr operand_;
+
   public:
-    Unary_operator(Unary_operators op, Expression_ptr operand) 
+    Unary_operator(Unary_operators op, Expression_ptr operand)
         : op_(op), operand_(std::move(operand)) {}
 
     Unary_operators get_operator() const { return op_; }
-    Expression   &get_operand()       { return *operand_; }
-    const Expression &get_operand() const  { return *operand_; }
+    Expression &get_operand() { return *operand_; }
+    const Expression &get_operand() const { return *operand_; }
 
     void accept(ASTVisitor &visitor) override { visitor.visit(*this); }
 };
@@ -226,4 +221,3 @@ class Variable : public Expression {
 } // namespace language
 
 #endif // FRONTEND_INCLUDE_AST_HPP
-
